1c2b05d4bc6b08eb586b158f0b95a3ea3d4949e6
msedlazek <mark.sedlazek@gmail.com>
2017-02-16 16:39:33 -0500

    put functioning code into function to be recursive when prompted.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,130 +10,71 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query('SELECT * from products', function (error, results, fields)
-{
-	console.log("Welcome to Bamazon! Here is our selection of products for today:")
-    for (var i = 0; i<results.length; i++){
-    	console.log("Item ID: " + results[i].id);
-    	console.log("Item Name: " + results[i].product_name);
-    	console.log("Price: $" + results[i].price);
-    	console.log("Quantity in Stock: " + results[i].stock_quantity);
-    	console.log("Department ID: " + results[i].department_id);
-    	console.log('=============================\n');
-    };
-    	inquirer.prompt([
-    		{type: "input",
-      		name: "product_id",
-      		message: "Please enter the Item ID of the product you wish to purchase."}
-    		]).then(function(data){
-        		var product = data.product_id - 1;
-        		console.log("You have selected " + results[product].product_name);
-        		
-        		inquirer.prompt([
-    				{type: "input",
-      				name: "units",
-      				message: "How much of this item would you like to purchase?"}
-    				]).then(function(data){
-    					var units = data.units
-    					var totalCost = units * results[product].price;
-    					var newCount = results[product].stock_quantity - units;
-    					if (newCount >= 0){
-    						console.log("Your selection of " + units + " " + results[product].product_name + " will have a total cost of $" + totalCost + ".");
-    						connection.query("UPDATE products SET stock_quantity=" + newCount + " WHERE id=" + results[product].id, function(err, res) { 
-            					if (err) return console.log(err);
-         					});
-         					connection.query("INSERT INTO sales (product_id, quantity_purchased) VALUES (" + results[product].id + ", " + units + " )", function(err, res) { 
-            					if (err) return console.log(err);
-          					});
-          					inquirer.prompt([
-    							{type: "input",
-      							name: "confirm",
-      							message: "Thank you for your purchase, would you like to make another one? yes/no"}
-    							]).then(function(data){
-    								if (data.confirm == "yes"){
-    									console.log("Then exit and try again dummy!")
-    								} else {
-    									console.log("Have a nice day!")
-    									connection.end();
-    								}
-    						});
-    					} else {
-    						inquirer.prompt([
-    							{type: "input",
-      							name: "confirm",
-      							message: "Insufficient quantity in stock! Would you like to try again? yes/no"}
-    							]).then(function(data){
-    								if (data.confirm == "yes"){
-    									console.log("Then exit and try again dummy!")
-    								} else {
-    									console.log("Have a nice day!")
-    									connection.end();
-    								}
-    						});
-    					};
-    			});
-        });
-});
+var runApp = function(){
+	connection.query('SELECT * from products', function (error, results, fields)
+	{
+		console.log("Welcome to Bamazon! Here is our selection of products for today:")
+	    for (var i = 0; i<results.length; i++){
+	    	console.log("Item ID: " + results[i].id);
+	    	console.log("Item Name: " + results[i].product_name);
+	    	console.log("Price: $" + results[i].price);
+	    	console.log("Quantity in Stock: " + results[i].stock_quantity);
+	    	console.log("Department ID: " + results[i].department_id);
+	    	console.log('=============================\n');
+	    };
+	    	inquirer.prompt([
+	    		{type: "input",
+	      		name: "product_id",
+	      		message: "Please enter the Item ID of the product you wish to purchase."}
+	    		]).then(function(data){
+	        		var product = data.product_id - 1;
+	        		console.log("You have selected " + results[product].product_name);
+	        		
+	        		inquirer.prompt([
+	    				{type: "input",
+	      				name: "units",
+	      				message: "How much of this item would you like to purchase?"}
+	    				]).then(function(data){
+	    					var units = data.units
+	    					var totalCost = units * results[product].price;
+	    					var newCount = results[product].stock_quantity - units;
+	    					if (newCount >= 0){
+	    						console.log("Your selection of " + units + " " + results[product].product_name + " will have a total cost of $" + totalCost + ".");
+	    						connection.query("UPDATE products SET stock_quantity=" + newCount + " WHERE id=" + results[product].id, function(err, res) { 
+	            					if (err) return console.log(err);
+	         					});
+	         					connection.query("INSERT INTO sales (product_id, quantity_purchased) VALUES (" + results[product].id + ", " + units + " )", function(err, res) { 
+	            					if (err) return console.log(err);
+	          					});
+	          					inquirer.prompt([
+	    							{type: "input",
+	      							name: "confirm",
+	      							message: "Thank you for your purchase, would you like to make another one? yes/no"}
+	    							]).then(function(data){
+	    								if (data.confirm == "yes"){
+	    									runApp();
+	    								} else {
+	    									console.log("Have a nice day!")
+	    									connection.end();
+	    								}
+	    						});
+	    					} else {
+	    						inquirer.prompt([
+	    							{type: "input",
+	      							name: "confirm",
+	      							message: "Insufficient quantity in stock! Would you like to try again? yes/no"}
+	    							]).then(function(data){
+	    								if (data.confirm == "yes"){
+	    									runApp();
+	    								} else {
+	    									console.log("Have a nice day!")
+	    									connection.end();
+	    								}
+	    						});
+	    					};
+	    			});
+	        });
+	});
+};
 
-// [ RowDataPacket {
-//     id: 1,
-//     product_name: 'leather jacket',
-//     department_id: 1,
-//     price: 124.95,
-//     stock_quantity: 20 },
-//   RowDataPacket {
-//     id: 2,
-//     product_name: 'cargo pants',
-//     department_id: 1,
-//     price: 34.6,
-//     stock_quantity: 25 },
-//   RowDataPacket {
-//     id: 3,
-//     product_name: 'white canvas',
-//     department_id: 2,
-//     price: 9.99,
-//     stock_quantity: 30 },
-//   RowDataPacket {
-//     id: 4,
-//     product_name: 'sketchbook',
-//     department_id: 2,
-//     price: 12.5,
-//     stock_quantity: 15 },
-//   RowDataPacket {
-//     id: 5,
-//     product_name: 'art pencils',
-//     department_id: 2,
-//     price: 19.9,
-//     stock_quantity: 20 },
-//   RowDataPacket {
-//     id: 6,
-//     product_name: 'set of acrylic paint',
-//     department_id: 2,
-//     price: 25.5,
-//     stock_quantity: 30 },
-//   RowDataPacket {
-//     id: 7,
-//     product_name: '"American Gods"',
-//     department_id: 3,
-//     price: 19.3,
-//     stock_quantity: 16 },
-//   RowDataPacket {
-//     id: 8,
-//     product_name: '"Leviathan Wakes"',
-//     department_id: 3,
-//     price: 14.65,
-//     stock_quantity: 10 },
-//   RowDataPacket {
-//     id: 9,
-//     product_name: '"Jennifer Government"',
-//     department_id: 3,
-//     price: 10.11,
-//     stock_quantity: 12 },
-//   RowDataPacket {
-//     id: 10,
-//     product_name: '"Coding in Java"',
-//     department_id: 3,
-//     price: 22.99,
-//     stock_quantity: 10 } ]
-
-// Note to self, maybe have separate connection query in listing of items to pull department name? Or run department query first and then products?
+runApp();
